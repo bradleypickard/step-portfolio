52004922acf1f336b9d23ca50ebb1e3116188774
Bradley <pickbradley23@gmail.com>
2020-06-22 22:17:05 +0000

    Make basic personalizations, add pet image

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['That\'s rough buddy', 'My cabbages!', 'Flameo, hotman', 'Who are you? And what do you want?'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
