ddb5b2a53a3a9d2f5190e498c0dc701599ed3924
Bradley <pickbradley23@gmail.com>
2020-06-23 23:27:49 +0000

    Implement 'About' section button functionality

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const about = 
+    [
+        'This is my background.',
+        'These are my interests.',
+        'These are some of my projects.'
+    ]
+
+/**
+ * Modifies the 'innertext' of a given element 
+ */
+function setContentTo(elementID, content) {
+  const container = document.getElementById(elementID);
+  container.innerText = content;
+}
+
 /**
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
   const greetings =
-      ['That\'s rough buddy', 'My cabbages!', 'Flameo, hotman', 'Who are you? And what do you want?'];
+      ['That\'s rough buddy', 'My cabbages!', 'Flameo, hotman!', 'Who are you? And what do you want?'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  setContentTo('greeting-container', greeting);
+}
+
+/**
+ * Selects the contents of the 'About' section.
+ */
+function setAboutContentTo(section) {
+  switch (section) {
+    case 'background':
+      content = about[0];
+      break;
+    case 'interests':
+      content = about[1];
+      break; 
+    case 'projects':
+      content = about[2];
+      break; 
+  }
+  
+  setContentTo('about-me-container', content);
 }
